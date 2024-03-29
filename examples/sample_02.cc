9c70bce49bc3205ffd69e47904317f544d361fe5
Gustav <sir.gustav.the.coder@gmail.com>
2024-03-29 23:02:56 +0100

    Rename some members

--- examples/sample_02.cc
+++ examples/sample_02.cc
@@ -9,13 +9,13 @@
 
 struct Sample : public App
 {
-	std::vector<ScalarTrack> mScalarTracks;
-	std::vector<bool> mScalarTracksLooping;
+	std::vector<ScalarTrack> scalar_tracks;
+	std::vector<bool> scalar_tracks_looping;
 
-	DebugDraw* mScalarTrackLines;
-	DebugDraw* mHandleLines;
-	DebugDraw* mHandlePoints;
-	DebugDraw* mReferenceLines;
+	DebugDraw* draw_track_lines;
+	DebugDraw* draw_handle_lines;
+	DebugDraw* draw_handle_points;
+	DebugDraw* draw_referene_lines;
 
 	ScalarFrame MakeFrame(float time, float value)
 	{
@@ -34,27 +34,27 @@ struct Sample : public App
 
 	Sample()
 	{
-		mScalarTracks.push_back(
+		scalar_tracks.push_back(
 			MakeScalarTrack(Interpolation::Linear, {MakeFrame(0.0f, 0.0f), MakeFrame(1.0f, 1.0f)})
 		);
-		mScalarTracksLooping.push_back(false);
+		scalar_tracks_looping.push_back(false);
 
-		mScalarTracks.push_back(
+		scalar_tracks.push_back(
 			MakeScalarTrack(Interpolation::Linear, {MakeFrame(0.0f, 0.0f), MakeFrame(0.5f, 1.0f)})
 		);
-		mScalarTracksLooping.push_back(false);
+		scalar_tracks_looping.push_back(false);
 
-		mScalarTracks.push_back(MakeScalarTrack(
+		scalar_tracks.push_back(MakeScalarTrack(
 			Interpolation::Linear,
 			{MakeFrame(0.25f, 0.0f), MakeFrame(0.5f, 1.0f), MakeFrame(0.75f, 0.0f)}
 		));
-		mScalarTracksLooping.push_back(true);
+		scalar_tracks_looping.push_back(true);
 
-		mScalarTracks.push_back(MakeScalarTrack(
+		scalar_tracks.push_back(MakeScalarTrack(
 			Interpolation::Linear,
 			{MakeFrame(0.25f, 0.0f), MakeFrame(0.5f, 1.0f), MakeFrame(0.75f, 0.0f)}
 		));
-		mScalarTracksLooping.push_back(false);
+		scalar_tracks_looping.push_back(false);
 
 		ScalarTrack stepTrack{{}, Interpolation::Constant};
 		stepTrack.frames.resize(11);
@@ -63,25 +63,25 @@ struct Sample : public App
 			float time = static_cast<float>(i) / 10.0f * 0.5f + 0.25f;
 			stepTrack.frames[i] = MakeFrame(time, (i % 2 == 0.0f) ? 0.0f : 1.0f);
 		}
-		mScalarTracks.push_back(stepTrack);
-		mScalarTracks.push_back(stepTrack);
-		mScalarTracksLooping.push_back(true);
-		mScalarTracksLooping.push_back(false);
+		scalar_tracks.push_back(stepTrack);
+		scalar_tracks.push_back(stepTrack);
+		scalar_tracks_looping.push_back(true);
+		scalar_tracks_looping.push_back(false);
 
-		mScalarTracks.push_back(MakeScalarTrack(
+		scalar_tracks.push_back(MakeScalarTrack(
 			Interpolation::Cubic,
 			{MakeFrame(0.25f, 0.676221f, 0.0f, 0.676221f),
 			 MakeFrame(0.75f, 4.043837f, 1.0f, 4.043837f)}
 		));
-		mScalarTracksLooping.push_back(false);
+		scalar_tracks_looping.push_back(false);
 
-		mScalarTracks.push_back(MakeScalarTrack(
+		scalar_tracks.push_back(MakeScalarTrack(
 			Interpolation::Cubic,
 			{MakeFrame(0.25f, 0, 0, 0), MakeFrame(0.5f, 0, 1, 0), MakeFrame(0.75f, 0, 0, 0)}
 		));
-		mScalarTracksLooping.push_back(true);
+		scalar_tracks_looping.push_back(true);
 
-		mScalarTracks.push_back(MakeScalarTrack(
+		scalar_tracks.push_back(MakeScalarTrack(
 			Interpolation::Cubic,
 			{MakeFrame(0.25f, 0, 0, 0),
 			 MakeFrame(0.3833333f, -10.11282f, 0.5499259f, -10.11282f),
@@ -89,9 +89,9 @@ struct Sample : public App
 			 MakeFrame(0.6333333f, 7.925411f, 0.4500741f, 7.925411f),
 			 MakeFrame(0.75f, 0, 0, 0)}
 		));
-		mScalarTracksLooping.push_back(true);
+		scalar_tracks_looping.push_back(true);
 
-		mScalarTracks.push_back(MakeScalarTrack(
+		scalar_tracks.push_back(MakeScalarTrack(
 			Interpolation::Cubic,
 			{MakeFrame(0.25f, 0, 0, 0),
 			 MakeFrame(0.3833333f, 13.25147f, 0.5499259f, -10.11282f),
@@ -99,12 +99,12 @@ struct Sample : public App
 			 MakeFrame(0.6333333f, 7.925411f, 0.4500741f, -11.40672f),
 			 MakeFrame(0.75f, 0, 0, 0)}
 		));
-		mScalarTracksLooping.push_back(true);
+		scalar_tracks_looping.push_back(true);
 
-		mScalarTrackLines = new DebugDraw();
-		mHandlePoints = new DebugDraw();
-		mHandleLines = new DebugDraw();
-		mReferenceLines = new DebugDraw();
+		draw_track_lines = new DebugDraw();
+		draw_handle_points = new DebugDraw();
+		draw_handle_lines = new DebugDraw();
+		draw_referene_lines = new DebugDraw();
 
 		// Range in both X and Y is 0 to 20
 
@@ -117,17 +117,17 @@ struct Sample : public App
 			float left = 1.0f;
 			float right = 14.0;
 
-			mReferenceLines->Push(vec3(left, yPosition, 0));
-			mReferenceLines->Push(vec3(left, yPosition + height, 0));
+			draw_referene_lines->Push(vec3(left, yPosition, 0));
+			draw_referene_lines->Push(vec3(left, yPosition + height, 0));
 
-			mReferenceLines->Push(vec3(left, yPosition, 0));
-			mReferenceLines->Push(vec3(right, yPosition, 0));
+			draw_referene_lines->Push(vec3(left, yPosition, 0));
+			draw_referene_lines->Push(vec3(right, yPosition, 0));
 		}
-		mReferenceLines->UpdateOpenGLBuffers();
+		draw_referene_lines->UpdateOpenGLBuffers();
 
-		// Safe to assume that mScalarTracks has a size of 10
+		// Safe to assume that scalar_tracks has a size of 10
 
-		for (unsigned int i = 0; i < mScalarTracks.size(); ++i)
+		for (unsigned int i = 0; i < scalar_tracks.size(); ++i)
 		{
 			float yPosition
 				= (static_cast<float>(9 - i) * 2.0f) + (static_cast<float>(9 - i) * 0.2f) + 0.1f;
@@ -145,31 +145,31 @@ struct Sample : public App
 				float thisX = left + thisJNorm * xRange;
 				float nextX = left + nextJNorm * xRange;
 
-				float thisY = mScalarTracks[i].get_sample(thisJNorm, mScalarTracksLooping[i]);
-				float nextY = mScalarTracks[i].get_sample(nextJNorm, mScalarTracksLooping[i]);
+				float thisY = scalar_tracks[i].get_sample(thisJNorm, scalar_tracks_looping[i]);
+				float nextY = scalar_tracks[i].get_sample(nextJNorm, scalar_tracks_looping[i]);
 
 				thisY = yPosition + thisY * height;
 				nextY = yPosition + nextY * height;
 
-				mScalarTrackLines->Push(vec3(thisX, thisY, 0.1f));
-				mScalarTrackLines->Push(vec3(nextX, nextY, 0.1f));
+				draw_track_lines->Push(vec3(thisX, thisY, 0.1f));
+				draw_track_lines->Push(vec3(nextX, nextY, 0.1f));
 			}
 
-			const auto numFrames = static_cast<unsigned int>(mScalarTracks[i].frames.size());
+			const auto numFrames = static_cast<unsigned int>(scalar_tracks[i].frames.size());
 			for (unsigned int j = 0; j < numFrames; ++j)
 			{
-				float thisTime = mScalarTracks[i].frames[j].time;
+				float thisTime = scalar_tracks[i].frames[j].time;
 				float thisY
 					= yPosition
-					+ mScalarTracks[i].get_sample(thisTime, mScalarTracksLooping[i]) * height;
+					+ scalar_tracks[i].get_sample(thisTime, scalar_tracks_looping[i]) * height;
 				float thisX = left + thisTime * xRange;
-				mHandlePoints->Push(vec3(thisX, thisY, 0.9f));
+				draw_handle_points->Push(vec3(thisX, thisY, 0.9f));
 
 				if (j > 0)
 				{
 					float prevY
 						= yPosition
-						+ mScalarTracks[i].get_sample(thisTime - 0.0005f, mScalarTracksLooping[i])
+						+ scalar_tracks[i].get_sample(thisTime - 0.0005f, scalar_tracks_looping[i])
 							  * height;
 					float prevX = left + (thisTime - 0.0005f) * xRange;
 
@@ -177,15 +177,15 @@ struct Sample : public App
 					vec3 prevVec = vec3(prevX, prevY, 0.6f);
 					vec3 handleVec = thisVec + get_normalized(prevVec - thisVec) * 0.75f;
 
-					mHandleLines->Push(thisVec);
-					mHandleLines->Push(handleVec);
+					draw_handle_lines->Push(thisVec);
+					draw_handle_lines->Push(handleVec);
 				}
 
-				if (j < numFrames - 1 && mScalarTracks[i].interpolation != Interpolation::Constant)
+				if (j < numFrames - 1 && scalar_tracks[i].interpolation != Interpolation::Constant)
 				{
 					float nextY
 						= yPosition
-						+ mScalarTracks[i].get_sample(thisTime + 0.0005f, mScalarTracksLooping[i])
+						+ scalar_tracks[i].get_sample(thisTime + 0.0005f, scalar_tracks_looping[i])
 							  * height;
 					float nextX = left + (thisTime + 0.0005f) * xRange;
 
@@ -193,25 +193,25 @@ struct Sample : public App
 					vec3 nextVec = vec3(nextX, nextY, 0.6f);
 					vec3 handleVec = thisVec + get_normalized(nextVec - thisVec) * 0.75f;
 
-					mHandleLines->Push(thisVec);
-					mHandleLines->Push(handleVec);
+					draw_handle_lines->Push(thisVec);
+					draw_handle_lines->Push(handleVec);
 				}
 			}
 		}
-		mScalarTrackLines->UpdateOpenGLBuffers();
-		mHandleLines->UpdateOpenGLBuffers();
-		mHandlePoints->UpdateOpenGLBuffers();
+		draw_track_lines->UpdateOpenGLBuffers();
+		draw_handle_lines->UpdateOpenGLBuffers();
+		draw_handle_points->UpdateOpenGLBuffers();
 	}
 
 	~Sample()
 	{
-		mScalarTracks.clear();
-		mScalarTracksLooping.clear();
+		scalar_tracks.clear();
+		scalar_tracks_looping.clear();
 
-		delete mScalarTrackLines;
-		delete mHandlePoints;
-		delete mHandleLines;
-		delete mReferenceLines;
+		delete draw_track_lines;
+		delete draw_handle_points;
+		delete draw_handle_lines;
+		delete draw_referene_lines;
 	}
 
 	void on_frame(float) override
@@ -226,10 +226,10 @@ struct Sample : public App
 
 		mat4 mvp = projection * view * model;
 
-		mReferenceLines->Draw(DebugDrawMode::Lines, vec3(1, 1, 1), mvp);
-		mScalarTrackLines->Draw(DebugDrawMode::Lines, vec3(0, 1, 0), mvp);
-		mHandleLines->Draw(DebugDrawMode::Lines, vec3(1, 0, 0), mvp);
-		mHandlePoints->Draw(DebugDrawMode::Points, vec3(0, 0, 1), mvp);
+		draw_referene_lines->Draw(DebugDrawMode::Lines, vec3(1, 1, 1), mvp);
+		draw_track_lines->Draw(DebugDrawMode::Lines, vec3(0, 1, 0), mvp);
+		draw_handle_lines->Draw(DebugDrawMode::Lines, vec3(1, 0, 0), mvp);
+		draw_handle_points->Draw(DebugDrawMode::Points, vec3(0, 0, 1), mvp);
 	}
 
 	void on_gui() override
